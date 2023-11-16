4dda4c155aabe3432970aead95e0e109b224a478
Manolo <krivoymanuel@gmail.com>
2023-11-16 18:38:05 -0300

    carga de mensajes a bdd

--- server/index.js
+++ server/index.js
@@ -9,56 +9,69 @@ const port = process.env.PORT;
 
 import { connection } from "./config.js";
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error al conectar a la base de datos: ", err);
-    return;
-  }
-  console.log("Conexión a la base de datos exitosa");
-  console.log(process.env.DATABASE_PORT);
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
-  // Consulta para crear la tabla 'messages'
-  const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS messages (
+// Manejamos errores al conectarnos a la base de datos
+async function connectToDatabase() {
+  try {
+    await connection.connect();
+    console.log("Conexión a la base de datos exitosa");
+
+    // Creamos la tabla 'messages' si no existe
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTO_INCREMENT,
         content VARCHAR(256)
       )
     `;
+    await connection.execute(createTableQuery);
+    console.log("Tabla 'messages' creada o ya existente");
 
-  connection.query(createTableQuery, (error, results) => {
-    if (error) {
-      console.error("Error al ejecutar la consulta:", error);
-    }
+    // Retornamos la conexión
+    return connection;
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+    throw error;
+  }
+}
 
-    const app = express();
-    const server = createServer(app); // Se necesita formatear a app como un servidor http formalmente para utilizarlo como parametro en io
-    const io = new Server(server, {
-      connectionStateRecovery: {}, //para no perder informacion, crea persistencia de datos, si un usuario se desconecta y luego se vuelve a conectar recibe los mensajes que deberia haber recibido estando desconectado.
-    }); // Esto permite la comunicación en tiempo real entre el servidor y los clientes a través de WebSockets.
+// Manejamos los eventos de conexión y desconexión de Socket.IO
+io.on("connection", (socket) => {
+  console.log("A user has connected!");
 
-    io.on("connection", (socket) => {
-      //IO SON TODAS LAS CONEXIONES, EL SOCKET UNA COMUNICACIÓN EN CONCRETO.
-      console.log("A user has connected!");
-
-      socket.on("disconnect", () => {
-        console.log("a user has disconnected");
-      });
-
-      socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
-      });
-    });
-
-    app.use(logger("dev"));
-
-    app.get("/", (req, res) => {
-      res.sendFile(process.cwd() + "/client/index.html");
-    });
-
-    server.listen(port, () => {
-      console.log("Server running on PORT: ", port);
-    });
-
-    connection.end();
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected");
   });
+
+  socket.on("chat message", async (msg) => {
+    try {
+      io.emit("chat message", msg);
+
+      // Conectamos a la base de datos
+      const connection = await connectToDatabase();
+
+      // Insertamos el mensaje en la base de datos
+      const insertMessageQuery = "INSERT INTO messages (content) VALUES (?)";
+      await connection.execute(insertMessageQuery, [msg]);
+      console.log("Message saved:", msg);
+
+      // Cerramos la conexión después de cada consulta
+     // await connection.end();
+    } catch (error) {
+      console.error("Error al insertar el mensaje en la base de datos:", error);
+    }
+  });
+});
+
+app.use(logger("dev"));
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
+
+server.listen(port, () => {
+  console.log("Server running on PORT:", port);
 });
