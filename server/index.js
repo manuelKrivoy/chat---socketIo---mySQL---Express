7668d736a346b2a82ec0f87f913039aecaf4ea03
Manolo <krivoymanuel@gmail.com>
2023-11-16 18:57:25 -0300

    mejora a manejo de conexiones bdd

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from "express";
-import logger from "morgan"; // Registra las solicitudes http que registra el servidor.
+import logger from "morgan";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 import dotenv from "dotenv";
 
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT ; // Utiliza el puerto especificado por la variable de entorno o 3000 por defecto
 
 import { connection } from "./config.js";
 
@@ -15,31 +15,35 @@ const io = new Server(server, {
   connectionStateRecovery: {},
 });
 
-// Manejamos errores al conectarnos a la base de datos
+let dbStatus = null;
+
+async function createMessagesTable(connection) {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTO_INCREMENT,
+      content VARCHAR(256)
+    )
+  `;
+  await connection.execute(createTableQuery);
+  console.log("Tabla 'messages' creada o ya existente");
+}
+
 async function connectToDatabase() {
   try {
-    await connection.connect();
-    console.log("Conexión a la base de datos exitosa");
+    if (!dbStatus) {
+      dbStatus = await connection.promise(); // Utiliza el método 'promise' para obtener una versión de la conexión compatible con promesas
+      console.log("Conexión a la base de datos exitosa");
 
-    // Creamos la tabla 'messages' si no existe
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS messages (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        content VARCHAR(256)
-      )
-    `;
-    await connection.execute(createTableQuery);
-    console.log("Tabla 'messages' creada o ya existente");
+      await createMessagesTable(dbStatus);
+    }
 
-    // Retornamos la conexión
-    return connection;
+    return dbStatus;
   } catch (error) {
     console.error("Error al conectar a la base de datos:", error);
     throw error;
   }
 }
 
-// Manejamos los eventos de conexión y desconexión de Socket.IO
 io.on("connection", (socket) => {
   console.log("A user has connected!");
 
@@ -51,16 +55,11 @@ io.on("connection", (socket) => {
     try {
       io.emit("chat message", msg);
 
-      // Conectamos a la base de datos
       const connection = await connectToDatabase();
 
-      // Insertamos el mensaje en la base de datos
       const insertMessageQuery = "INSERT INTO messages (content) VALUES (?)";
       await connection.execute(insertMessageQuery, [msg]);
       console.log("Message saved:", msg);
-
-      // Cerramos la conexión después de cada consulta
-     // await connection.end();
     } catch (error) {
       console.error("Error al insertar el mensaje en la base de datos:", error);
     }
